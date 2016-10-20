77c0d642c4a0ae282ebefcfe8a7777a98e01cf94
tasika696 <tasika696@DESKTOP-2JQIAFV>
2016-10-20 09:18:42 +0200

    Entity framework sajt user

---
+++ VPBusz/DAL/UserContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using VPBusz.Models;
+
+namespace VPBusz.DAL
+{
+    public class UserContext:DbContext
+    {
+        public UserContext(): base("UserContext")
+        {
+            
+        }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
