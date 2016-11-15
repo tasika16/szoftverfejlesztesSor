30bda6860998b7cc86f6e300030fd0d20aed3590
tasika16 <tasika696@gmail.com>
2016-11-15 17:28:14 +0100

    asd

--- VPBusz/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VPBusz.MvcApplication" Language="C#" %>
--- VPBusz/public/app/js/controllers/dashboard.js
+++
@@ -1,9 +0,0 @@
-app.controller('DashboardController', function($scope, $rootScope, NgMap, stopService){
-	$rootScope.pageTitle = 'Főoldal';
-
-	$scope.stopList = [];
-	stopService.getList().then(function(result){
-		$scope.stopList = result;
-		console.log(result);
-	});
-});
