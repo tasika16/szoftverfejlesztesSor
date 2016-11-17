cec097767c45f978a95e79c9f754e21d983ad221
Doink <wwfdoink@gmail.com>
2016-11-17 10:42:07 +0100

    fix db config

--- Startup.cs
+++ Startup.cs
@@ -9,11 +9,24 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using VPBusz.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace VPBusz
 {
     public class Startup
     {
+		public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+		
+		public IConfigurationRoot Configuration { get; }
+		
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,8 +34,8 @@ namespace VPBusz
             /*services.AddDbContext<VPBuszContext>(options => 
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnecntion")));*/
             services.AddMvc();
-            /*services.AddDbContext<VPBuszContext>(options => options.UseSqlServer
-            (Configuration.GetConnectionString("DefaultConnection")));*/
+            services.AddDbContext<VPBuszContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
